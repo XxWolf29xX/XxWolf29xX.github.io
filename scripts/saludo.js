0963fb620ffebdb46cd1183c8b1037f40ded4a0e
XxWolf29xX <140122267+XxWolf29xX@users.noreply.github.com>
2023-09-03 23:47:44 -0300

    Add files via upload

--- scripts/saludo.js
+++ scripts/saludo.js
@@ -8,6 +8,6 @@ function saludo() {
         sessionStorage.setItem("nombre",nombreaux);
         nombre = sessionStorage.getItem("nombre");
         }
-        alert("Bienvenido "+nombre)
+    alert("Bienvenido "+nombre)
 }
 saludo()
